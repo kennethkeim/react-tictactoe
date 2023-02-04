4df2217b5c3fc1743221c12b7daf9a5d651fe3c4
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:41:11 -0500

    refactor: rename app and add needed newline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Tictactoe from "./Tictactoe/Tictactoe";
 
-export default function Square() {
+export default function App() {
   return (
     <div className="page">
       <main>
