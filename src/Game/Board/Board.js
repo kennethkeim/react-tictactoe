b9fbb870309775c4ca9d1274c5c01409ce98cfbd
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:41:11 -0500

    wip: add name to button

--- src/Game/Board/Board.js
+++ src/Game/Board/Board.js
@@ -45,6 +45,7 @@ export default function Board({ xIsNext, squares, onPlay }) {
           {row.map((cell) => (
             <Square
               key={cell}
+              name={cell + 1}
               value={squares[cell]}
               onClick={() => handleSquareClick(cell)}
             ></Square>
--- src/Game/Square/Square.js
+++ src/Game/Square/Square.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 // React convention: use on[Event] names for props which represent events
-export default function Square({ value, onClick }) {
+export default function Square({ value, onClick, name }) {
   return (
-    <button onClick={onClick} className="square">
+    <button onClick={onClick} className="square" name={name}>
       {value}
     </button>
   );
