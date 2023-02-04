8249223686333c6934acaf86582e0252a4ddebbb
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:41:11 -0500

    wip: move square state handling to board

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import "./Board.css";
 
-function Square() {
+function Square({ value, onSquareClick }) {
   const inactiveBg = "#d5b85a";
   const activeBg = "#fcd12a";
-  const [value, setValue] = useState("");
   const [bg, setBg] = useState(inactiveBg);
 
   function onClick() {
-    setValue("X");
+    onSquareClick();
     setBg(activeBg);
   }
 
@@ -16,7 +15,7 @@ function Square() {
     <button
       onClick={onClick}
       className="square"
-      style={{ "background-color": bg }}
+      style={{ backgroundColor: bg }}
     >
       {value}
     </button>
@@ -24,22 +23,57 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleSqClick(i) {
+    const newSquareArr = [...squares];
+    newSquareArr[i] = "X";
+    setSquares(newSquareArr);
+  }
+
   return (
     <article>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleSqClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleSqClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleSqClick(2)}
+        ></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleSqClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleSqClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleSqClick(5)}
+        ></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleSqClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleSqClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleSqClick(8)}
+        ></Square>
       </div>
     </article>
   );
