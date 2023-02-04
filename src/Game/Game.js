1fd77c194320d136decaa8b80ec8b61ae673228f
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:41:11 -0500

    refactor: iterate over arr of arr to render rows, cells

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -46,51 +46,26 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? x : o);
   }
 
+  const rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
   return (
     <article>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square
-          value={squares[0]}
-          onSquareClick={() => handleSquareClick(0)}
-        ></Square>
-        <Square
-          value={squares[1]}
-          onSquareClick={() => handleSquareClick(1)}
-        ></Square>
-        <Square
-          value={squares[2]}
-          onSquareClick={() => handleSquareClick(2)}
-        ></Square>
-      </div>
-      <div className="board-row">
-        <Square
-          value={squares[3]}
-          onSquareClick={() => handleSquareClick(3)}
-        ></Square>
-        <Square
-          value={squares[4]}
-          onSquareClick={() => handleSquareClick(4)}
-        ></Square>
-        <Square
-          value={squares[5]}
-          onSquareClick={() => handleSquareClick(5)}
-        ></Square>
-      </div>
-      <div className="board-row">
-        <Square
-          value={squares[6]}
-          onSquareClick={() => handleSquareClick(6)}
-        ></Square>
-        <Square
-          value={squares[7]}
-          onSquareClick={() => handleSquareClick(7)}
-        ></Square>
-        <Square
-          value={squares[8]}
-          onSquareClick={() => handleSquareClick(8)}
-        ></Square>
-      </div>
+      {rows.map((row, i) => (
+        <div className="board-row" key={`row-${i}`}>
+          {row.map((cell) => (
+            <Square
+              key={cell}
+              value={squares[cell]}
+              onSquareClick={() => handleSquareClick(cell)}
+            ></Square>
+          ))}
+        </div>
+      ))}
     </article>
   );
 }
