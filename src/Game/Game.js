4aba3f7599ae07a8d1d32672fa81a7e6b9e0e95e
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:41:11 -0500

    wip: implement "go to" feature

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -101,20 +101,42 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(newSquareArr) {
+    const historyPrecedingCurrentPlay = history.slice(0, currentMove + 1);
+    const nextHistory = [...historyPrecedingCurrentPlay, newSquareArr];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
-    setHistory([...history, newSquareArr]);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
       <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} />
 
       <aside className="game-info">
-        <p>Status: </p>
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </aside>
     </div>
   );
