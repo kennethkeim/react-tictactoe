b4ab8b2023741a0092865b0fc8c05b946a4de14c
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:41:11 -0500

    fix: calculate winner path

--- src/Game/Board/Board.js
+++ src/Game/Board/Board.js
@@ -1,4 +1,4 @@
-import { calculateWinner } from "../../shared/calculateWinner";
+import { calculateWinner } from "../shared/calculateWinner";
 import React from "react";
 import Square from "../Square/Square";
 
